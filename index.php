a3b1dc5cac4706f85af6d25efb247184fd97bac6
Derni300 <alexou.debroissia@gmail.com>
2022-02-14 00:25:52 +0100

    update

--- index.php
+++ index.php
@@ -2,9 +2,13 @@
 <html lang="fr">
     <head>
         <meta charset="utf-8">
-        <link rel="stylesheet" href="styled.css">
+        <link rel="stylesheet" href="style.css">
         <script src="./script.js"></script>
         <title>Accueil</title>
+        <style>
+        body{ font: 14px sans-serif; text-align: center; }
+        .wrapper{ width: 360px; padding: 20px; margin: 0 auto;}
+        </style>
     </head>
         
     <body>
@@ -12,7 +16,8 @@
             <input type="text" id="positionX" size="1" disabled/>
             <input type="text" id="positionY" size="1" disabled/>
         </a>
-        <a href="login.php">Produitjjj</a> 
+        <br><br><br>
+        <a href="login.php">Explorer</a> 
     </body>
 
 </html>
--- register.php
+++ register.php
@@ -102,8 +102,8 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     <title>Sign Up</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <style>
-        body{ font: 14px sans-serif; }
-        .wrapper{ width: 360px; padding: 20px; }
+        body{ font: 14px sans-serif; text-align: center; }
+        .wrapper{ width: 360px; padding: 20px; margin: 0 auto; }
     </style>
 </head>
 <body>
