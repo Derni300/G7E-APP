5f7f0bd8c9406cd1d08097b002596ac6c7a293f7
Alexandre DE FROISSARD DE BROISSIA <78043623+Derni300@users.noreply.github.com>
2022-05-04 16:21:31 +0200

    rectified

--- register.php
+++ register.php
@@ -100,28 +100,25 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 <head>
     <meta charset="UTF-8">
     <title>Sign Up</title>
-    <style>
-        body{ font: 14px sans-serif; text-align: center; }
-        .wrapper{ width: 360px; padding: 20px; margin: 0 auto; }
-    </style>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <div class="wrapper">
-        <h2>Sign Up</h2>
+        <h2>S'enregister</h2>
         <p>Please fill this form to create an account.</p>
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
             <div class="form-group">
-                <label>Username</label>
+                <label>Pseudonyme</label>
                 <input type="text" name="username" class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>">
                 <span class="invalid-feedback"><?php echo $username_err; ?></span>
             </div>    
             <div class="form-group">
-                <label>Password</label>
+                <label>Mot de passe</label>
                 <input type="password" name="password" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $password; ?>">
                 <span class="invalid-feedback"><?php echo $password_err; ?></span>
             </div>
             <div class="form-group">
-                <label>Confirm Password</label>
+                <label>Confirmer le mot de passe</label>
                 <input type="password" name="confirm_password" class="form-control <?php echo (!empty($confirm_password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $confirm_password; ?>">
                 <span class="invalid-feedback"><?php echo $confirm_password_err; ?></span>
             </div>
@@ -129,7 +126,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                 <input type="submit" class="btn btn-primary" value="Submit">
                 <input type="reset" class="btn btn-secondary ml-2" value="Reset">
             </div>
-            <p>Already have an account? <a href="login.php">Login here</a>.</p>
+            <p>Déjà un compte ? <a href="login.php">Se connecter ici</a>.</p>
         </form>
     </div>    
 </body>
